b9dbcd3f7bdb3f484b91397876d36ff4f1534efc
canh1082003 <anhnbk113@gmail.com>
2025-06-12 13:34:33 +0700

    update 2 bai selenium

---
+++ type.py
@@ -0,0 +1,13 @@
+
+arr_userName = [
+    'standard_user',
+    'locked_out_user',
+    'problem_user',
+    'performance_glitch_user',
+    'error_user',
+    'visual_user'
+]
+
+PASSWORD = "secret_sauce"
+LOGIN_URL = "https://www.saucedemo.com/"
+BASE_URL="https://thuvienphapluat.vn/ma-so-thue/tra-cuu-ma-so-thue-doanh-nghiep"
